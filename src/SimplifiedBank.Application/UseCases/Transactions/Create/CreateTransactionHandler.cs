a09e808bc2d5a652794179c09af8eba32668051b
Pedro Lucas <pedrogmobile2@gmail.com>
2025-07-02 13:29:57 -0300

    improve(Application): implementa melhorias na criação da transação, incluindo operações de crédito/débito no saldo dos usuários

--- src/SimplifiedBank.Application/UseCases/Transactions/Create/CreateTransactionHandler.cs
+++ src/SimplifiedBank.Application/UseCases/Transactions/Create/CreateTransactionHandler.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using SimplifiedBank.Application.Shared.Responses;
 using SimplifiedBank.Domain.Entities;
+using SimplifiedBank.Domain.Enums;
 using SimplifiedBank.Domain.Exceptions;
 using SimplifiedBank.Domain.Interfaces;
 
@@ -23,12 +24,29 @@ public class CreateTransactionHandler : IRequestHandler<CreateTransactionRequest
     {
         var sender = await _userRepository.GetByIdAsync(request.SenderId, cancellationToken);
         var receiver = await _userRepository.GetByIdAsync(request.ReceiverId, cancellationToken);
-
+        
         if (sender is null || receiver is null)
             throw new UserNotFoundException("Não foi possível encontrar o Pagador/Recebedor.");
 
-        var transaction = await _transactionRepository.CreateAsync(request, cancellationToken);
+        if (sender.Type == EUserType.Shopkeeper)
+            throw new ShopkeeperCannotTransferException("Usuários lojistas não podem realizar transações, apenas recebê-las.");
         
+        // Adicionar serviço validador externo e garantia de unicidade depois
+        
+        var value = Math.Round(request.Value, 2); // arredondando o valor para 2 casas decimais
+        
+        //Realizando transação
+        sender.Debit(value);
+        receiver.Credit(value);
+        
+        var transaction = Transaction.Create(
+            sender.Id, 
+            receiver.Id, 
+            value);
+        await _transactionRepository.CreateAsync(transaction, cancellationToken);
+        
+        await _userRepository.UpdateAsync(sender, cancellationToken);
+        await _userRepository.UpdateAsync(receiver, cancellationToken);
         await _unitOfWork.CommitAsync(cancellationToken);       
         
         return transaction;       
--- src/SimplifiedBank.Application/UseCases/Transactions/Create/CreateTransactionValidator.cs
+++ src/SimplifiedBank.Application/UseCases/Transactions/Create/CreateTransactionValidator.cs
@@ -10,9 +10,11 @@ public class CreateTransactionValidator : AbstractValidator<CreateTransactionReq
     {
         RuleFor(transaction => new { transaction.SenderId, transaction.ReceiverId })
             .NotEmpty()
-            .WithMessage("O id do Pagador/Recebedor é obrigatório.")
-            .Must(id => Guid.TryParse(id.ToString(), out _))
-            .WithMessage("Insira um id válido.");
+            .WithMessage("O ID do Pagador/Recebedor é obrigatório.")
+            .Must(t => Guid.TryParse(t.SenderId.ToString(), out _))
+            .WithMessage("Insira um ID válido para o pagador.")
+            .Must(t => Guid.TryParse(t.ReceiverId.ToString(), out _))
+            .WithMessage("Insira um ID válido para o recebedor.");
 
         RuleFor(transaction => new { transaction.SenderId, transaction.ReceiverId })
             .Must((transaction, ids) => transaction.SenderId != transaction.ReceiverId)
@@ -22,7 +24,7 @@ public class CreateTransactionValidator : AbstractValidator<CreateTransactionReq
             .NotEmpty()
             .WithMessage("O valor da transferência não pode ser vazio.")
             .GreaterThan(DomainConfiguration.MinTransactionValue)
-            .WithMessage("O valor não pode ser negativo ou igual a zero.")
+            .WithMessage($"O valor não pode ser menor que {DomainConfiguration.MinTransactionValue}.")
             .LessThanOrEqualTo(DomainConfiguration.MaxTransactionValue)
             .WithMessage($"O valor da transação não pode ser maior que {DomainConfiguration.MaxTransactionValue}.");
     }
